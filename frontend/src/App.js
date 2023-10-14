e4fcff6170c1f065195d5b33a51f165943ed142b
unknown <22424018@student.hcmus.edu.vn>
2023-10-15 00:02:13 +0700

    bo sung tinh nang tim kiem san pham

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,15 +26,15 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/order/:id' component={OrderScreen} />
-          <Route path='/shipping' component={ShippingScreen} />
-          <Route path='/payment' component={PaymentScreen} />
-          <Route path='/placeorder' component={PlaceOrderScreen} />
+          {/* <Route path='/order/:id' component={OrderScreen} /> */}
+          {/* <Route path='/shipping' component={ShippingScreen} /> */}
+          {/* <Route path='/payment' component={PaymentScreen} /> */}
+          {/* <Route path='/placeorder' component={PlaceOrderScreen} /> */}
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/profile' component={ProfileScreen} />
           <Route path='/product/:id' component={ProductScreen} />
-          <Route path='/cart/:id?' component={CartScreen} />
+          {/* <Route path='/cart/:id?' component={CartScreen} /> */}
           <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route
